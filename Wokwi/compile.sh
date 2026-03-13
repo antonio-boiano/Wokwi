e051d3f7e78c3b793d34f836dda37a7173f0532c
Antonio Boiano <antonio.boiano@polimi.it>
2026-03-13 11:36:24 +0100

    generci executing bash

--- Wokwi/compile.sh
+++ Wokwi/compile.sh
@@ -11,7 +11,7 @@ if [ ! -d "$USERINPUT_FOLDER" ]; then
 fi
 
 # Activate PlatformIO virtual environment
-source /home/iotpolimi/.platformio/penv/bin/activate
+source ~/.platformio/penv/bin/activate
 
 # Change to the specified directory
 cd "$USERINPUT_FOLDER" || exit
